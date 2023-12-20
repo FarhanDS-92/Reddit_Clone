f0fd27d033434b1bd5f972ec153fdfb7a6302970
FarhanDS-92 <108830582+FarhanDS-92@users.noreply.github.com>
2023-12-20 15:10:54 -0500

    cookie

--- src/app/api/users/register/route.js
+++ src/app/api/users/register/route.js
@@ -1,11 +1,16 @@
 import { prisma } from "@/lib/prisma.js";
 import { NextResponse } from "next/server.js";
+import { cookies } from "next/headers";
+import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 
 export async function POST(request, response) {
   try {
+    // cookies , username, password
+    const cookieStore = cookies();
     const { username, password } = await request.json();
 
+    // Check for if they provided a username and password
     if (!username || !password) {
       return NextResponse.json({
         success: false,
@@ -13,8 +18,7 @@ export async function POST(request, response) {
       });
     }
 
-    const hashedPassword = await bcrypt.hash(password, 10);
-
+    // check for if the provided username is already used
     const checkUsername = await prisma.user.findFirst({
       where: {
         username,
@@ -28,6 +32,9 @@ export async function POST(request, response) {
       });
     }
 
+    // hashing password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
     const user = await prisma.user.create({
       data: {
         username,
@@ -35,7 +42,12 @@ export async function POST(request, response) {
       },
     });
 
-    delete user.password;
+    // setting JWT and putting a cookie with that
+    const token = jwt.sign(
+      { userId: user.id, username },
+      process.env.JWT_SECRET
+    );
+    cookieStore.set("token", token);
 
     return NextResponse.json({ success: true, user });
   } catch (error) {
---
+++ src/lib/fetchUser.js
@@ -0,0 +1,24 @@
+import jwt from "jsonwebtoken";
+import { cookies } from "next/headers.js";
+import { prisma } from "./prisma.js";
+
+export async function fetchUser() {
+  try {
+    const cookieStore = cookies();
+    const userCookie = cookieStore.get("token");
+
+    if (!userCookie) {
+      return {};
+    }
+
+    const decodedToken = jwt.verify(userCookie.value, process.env.JWT_SECRET);
+    const { userId } = decodedToken;
+    const user = await prisma.user.findFirst({ where: { id: userId } });
+    delete user.password;
+
+    return user;
+  } catch (error) {
+    console.log(error);
+    return {};
+  }
+}
