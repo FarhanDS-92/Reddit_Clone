a460e51bf135626c5014efd46bb9a37d6ade9d5e
FarhanDS-92 <108830582+FarhanDS-92@users.noreply.github.com>
2023-12-22 22:43:56 -0500

    starting subreddit

--- src/app/(pages)/subreddits/page.jsx
+++ src/app/(pages)/subreddits/page.jsx
@@ -1,7 +1,23 @@
-export default function Subreddits() {
+import { prisma } from "@/lib/prisma.js";
+
+export default async function Subreddits() {
+  const subreddits = await prisma.subReddit.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  console.log(subreddits);
   return (
     <section id="subredditSection" aria-label="Subreddit Page" role="region">
-      <p>Subreddit</p>
+      <button id="createSubreddit">+ Create A Subreddit</button>
+      {subreddits.map((subReddit) => {
+        return (
+          <div key={subReddit.id} className="defaultSubReddit">
+            {subReddit.name}
+          </div>
+        );
+      })}
     </section>
   );
 }
